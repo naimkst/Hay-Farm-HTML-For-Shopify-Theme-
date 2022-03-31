36333ab8e18ab5bd31f0885da67514bbd7478dc1
gobindasarkaritech <gobindasarkar@itech-softsolutions.com>
2022-03-31 19:19:38 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,90 @@
+(function ($) {
+  "use strict";
+  /*-------------------------------------------
+  preloader active
+  --------------------------------------------- */
+  jQuery(window).on('load', function () {
+    jQuery('.preloader').fadeOut('slow');
+  });
+
+  /*-------------------------------------------
+  Sticky Header
+  --------------------------------------------- */
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() > 80) {
+      $('#sticky').addClass('stick');
+    } else {
+      $('#sticky').removeClass('stick');
+    }
+  });
+
+  jQuery(document).ready(function () {
+    /*-------------------------------------------
+    js scrollup
+    --------------------------------------------- */
+    $.scrollUp({
+      scrollText: '<i class="fa fa-angle-up"></i>',
+      easingType: 'linear',
+      scrollSpeed: 300,
+      animation: 'fade'
+    });
+    /*-------------------------------------------
+    advertisment-banner-slide active
+    --------------------------------------------- */
+    $('.advertisment-banner-slide').owlCarousel({
+      loop: true,
+      margin: 30,
+      dots: false,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1
+        },
+        480: {
+          items: 2
+        },
+        600: {
+          items: 3
+        },
+        1000: {
+          items: 5
+        }
+      }
+    })
+    /*-------------------------------------------
+    products-slide active
+    --------------------------------------------- */
+    $('.products-slide').owlCarousel({
+      loop: true,
+      margin: 30,
+      dots: true,
+      nav: true,
+      navText: ['<i class="nav-btn fas fa-angle-left"></i>','<i class="nav-btn fas fa-angle-right"></i>'],
+      responsive: {
+        0: {
+          items: 1
+        },
+        414: {
+          items: 2
+        },
+        768: {
+          items: 3
+        },
+        1000: {
+          items: 4
+        },
+        1200: {
+          items: 5
+        }
+      }
+    })
+    /*-------------------------------------------
+    js counterUp
+    --------------------------------------------- */
+    $('.counter').counterUp({
+      delay: 10,
+      time: 1000
+    });
+  });
+
+})(jQuery);
